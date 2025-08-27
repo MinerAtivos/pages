68f91d8e7093e5cc323d0587a712570510be2cd3
MinerAtivos <151392603+MinerAtivos@users.noreply.github.com>
2025-08-27 15:21:59 -0300

    Update script.js

--- docs/blog/script.js
+++ docs/blog/script.js
@@ -53,13 +53,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
     loadPosts();
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleButton = document.getElementById('menu-toggle');
-  const mobileMenu = document.getElementById('mobile-menu');
-
-  toggleButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-  });
-});
-
