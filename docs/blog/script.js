029d7eb3bb6945d2c7cfeb4f8f48854954475612
MinerAtivos <151392603+MinerAtivos@users.noreply.github.com>
2025-08-27 14:22:40 -0300

    Update script.js

--- docs/blog/script.js
+++ docs/blog/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const postsGrid = document.querySelector('.posts-grid');
-    const postsJsonPath = 'blog/posts.json'; // Caminho para o seu arquivo JSON de posts
+    const postsJsonPath = 'posts.json'; // Caminho para o seu arquivo JSON de posts
 
     // Função para criar um card de post
     function createPostCard(post) {
